b1443707262552a6b7c40e8d8b0faa74cf739e56
DM-hash-del <mackiedavidyt@gmail.com>
2026-03-27 01:15:39 +0000

    side menu on all pages added, tidied up styling, added jQuery and JS logic

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,41 @@
-// $(document).ready(function() {
-//   $("#left-menu").sidr();
-// })
+// ? Sometimes no-scroll works, sometimes it doesn't?!
+// let currentScrollPosition = 0;
+
+$(document).ready(function() {
+  // call plugin on element => specify which element to control
+  $("#mobile-menu").sidr({
+    name: "side-menu", // useful if trigger ins't <a> => <button> | <div>
+    side: "left",
+    displace: false,
+    onOpen: function() {
+      // currentScrollPosition = $(window).scrollTop();
+      $("body").addClass("no-scroll");
+      $("html").addClass("no-scroll");
+      $(".burger-container").fadeOut(200);
+    },
+    onClose: function() {
+      // $(window).scrollTop(currentScrollPosition);
+      $("body").removeClass("no-scroll");
+      $("html").removeClass("no-scroll");
+      $(".burger-container").fadeIn(300);
+    },
+  });
+  
+  $(".close-sidr").on('click', () => {
+    $.sidr("close", "side-menu");
+  });
+  
+  $("#side-menu").on("click", "a[href^='#']", function() {
+    $.sidr("close", "side-menu");
+  });
+
+  $(window).on("click", function(event) {
+    let clickedBurger = $(event.target).closest(".burger-container").length > 0;
+    let clickedSidemenu = $(event.target).closest(".side-bar").length > 0;
+    let isMenuOpen = $("body").hasClass("sidr-open");
+
+    if (!clickedBurger && !clickedSidemenu && isMenuOpen) {
+      $.sidr("close", "side-menu");
+    }
+  });
+});
