91770a1a30b3837329160d61d51252d041bad7b8
DM-hash-del <mackiedavidyt@gmail.com>
2026-03-31 11:58:10 +0100

    commented code

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,6 @@
 // let currentScrollPosition = 0;
 $(window).ready(function() {
 
-
   // call plugin on element => specify which element to control
   $("#mobile-menu").sidr({
     name: "side-menu", // useful if trigger ins't <a> => <button> | <div>
@@ -60,20 +59,21 @@ $(window).ready(function() {
 
 
   function validateInput($input) {
-    // These are not the original regex patterns, they adpated for JS
+    // These are not the original regex patterns, adapted for JS
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const phoneRegex = /((?:\+|00)[17](?: |-)?|(?:\+|00)[1-9]\d{0,2}(?: |-)?|(?:\+|00)1-\d{3}(?: |-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |-)[0-9]{3}(?: |-)[0-9]{4})|([0-9]{7}))/;
 
     let isvalid = true;
+    // subject not required, has no value, fix?
     const value = $input.val().trim();
     const isRequired = $input.prop("required");
     const type = $input.attr("type");
-    console.log("VALUE: ", value, "\n", "ISREQURED: ", isRequired, "\n", "TYPE: ", type);
 
     // check all input values against types
     if (isRequired && value === "") {
       isvalid = false;
     } else if (value !== "") {
+      // false => true => false
       if (type === "email" && !emailRegex.test(value)) {
         isvalid = false;
       } else if (type === "tel" && !phoneRegex.test(value)) {
@@ -89,13 +89,11 @@ $(window).ready(function() {
       $input.removeClass("contact__input--valid").removeClass("contact__input--invalid");
     }
 
-    console.log("ISVALID RETURN VALUE: ", isvalid);
     return isvalid;
   };
 
 
   $(".contact__input").on("blur", function() {
-    console.log("$(THIS) VALUE ON BLUR: ", $(this));
     validateInput($(this))
   });
 
@@ -117,6 +115,7 @@ $(window).ready(function() {
 // Animation function, per-string.
 function typeWriter(el, str, delay) {
   el.addClass("tw-bar");
+  // Once the recursion is done return promise => chain
   return new Promise((res) => {
     let increment = 0;
     function nextChar() {
